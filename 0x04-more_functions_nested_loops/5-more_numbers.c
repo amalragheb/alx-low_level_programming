b758e063f4b5230a7cfa07b86c6b0747c62decb6
Amal Ragheb <je.amal985@gmail.com>
2023-02-23 13:11:09 +0200

    mandatory

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,19 @@
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int num, row, count;
 
-	for (i = 0; i <= 10; i++)
+	for (row = 1; row <= 10; ++row)
 	{
-		for (j = 0; j <= 14; j++)
+		for (count = 0; count <= 14; ++count)
 		{
-			_putchar(j);
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
 		_putchar('\n');
 	}
