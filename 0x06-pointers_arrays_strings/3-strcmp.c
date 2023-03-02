760dc0449f8dcca0492866533d29f86adecf1456
Amal Ragheb <je.amal985@gmail.com>
2023-03-02 11:17:51 +0200

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+/**
+ * _strcmp - writes the character c to stdout
+ * @s1: The character to print
+ * @s2: dsfsdfsdfsdfd
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			return (0);
+		else
+			return (s1 - s2);
+		i++
+	}
+}
