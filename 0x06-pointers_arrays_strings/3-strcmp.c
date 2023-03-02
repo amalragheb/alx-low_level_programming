e6cbbe752a8e59fa3d9768b34205ed63137d8379
Amal Ragheb <je.amal985@gmail.com>
2023-03-02 11:31:26 +0200

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,35 @@
 /**
- * _strcmp - writes the character c to stdout
- * @s1: The character to print
- * @s2: dsfsdfsdfsdfd
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * _strcmp - a function that compares two strings
+ *
+ * @s1: string 1 input to compare
+ * @s2: against this other string 2
+ *
+ * Return: 0 if s1 and s2 are equal
+ *        negative integer if the stopping character
+ *                in @s1 was less than the stopping
+ *                character in @s2
+ *        positive integer if the stopping character
+ *                in @s1 was greater than the stopping
+ *                character in @s2
+*/
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, res;
+	int i = 0, r;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			res = s1[i] - s2[i];
+			r = s1[i] - s2[i];
 			break;
 		}
 		else
-			res = s1[i] - s2[i];
-		i++
+		{
+			r = s1[i] - s2[i];
+		}
+		i++;
 	}
 
-	return (res);
+	return (r);
 }
