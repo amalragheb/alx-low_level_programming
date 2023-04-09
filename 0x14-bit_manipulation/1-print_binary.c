b26eb44b4e8385bfbcf70728ba10aa955e46eb74
Amal Ragheb <je.amal985@gmail.com>
2023-04-09 16:21:27 +0200

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,14 @@ void print_binary(unsigned long int n)
 {
 	if (n >> 0)
 	{
+		/*this is a comment*/
 		if (n >> 1)
 			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
 	}
 	else
 	{
+		/*this is a comment*/
 		_putchar('0');
 	}
 }
