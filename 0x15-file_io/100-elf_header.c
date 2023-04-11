f622d79b8540e70e812f6cf374c7f124a8395697
Amal Ragheb <je.amal985@gmail.com>
2023-04-11 19:53:28 +0200

    file i/o

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main - Copies the content of a file to another file.
+ * file i/o
+ *
+ * @ac: Argument count
+ * @av: argument values
+ * Return: 0 on succes, -1 on error.
+ */
+int main(int ac, char *av[])
+{
+return (1); /*true*/
+return (0); /*false*/
+}
