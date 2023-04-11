3762d4c216a7214af869e2541ba5cd6d888418eb
Amal Ragheb <je.amal985@gmail.com>
2023-04-11 19:38:20 +0200

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,10 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t o, r, w;
 	char *buffer;
 
-	if (filename == NULL)
+	if (filename == NULL) /*this is a comment*/
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters); /*comment*/
 	if (buffer == NULL)
 		return (0);
 
@@ -37,5 +37,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 	close(o);
 
-	return (w);
+	return (w); /*return*/
 }
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success OR -1 on faliure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, length = 0;
+
+	if (filename == NULL) /*this is a comment*/
+		return (-1);
+	
+	/*this is comment*/
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	/*a new comment*/
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, length);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1); /*return*/
+}
