86a4e4fc7a4555b74929445f8d729ffb75be52e2
root <root@DESKTOP-1QK7UT6.localdomain>
2023-05-08 19:31:07 +0300

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	opened = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	opening = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (opening == -1)
 		return (-1);
 	if (text_content == NULL)
